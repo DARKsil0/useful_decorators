321670226f9595c8830f12ca772dcfdc71138dd0
DARKsil0 <122098172+DARKsil0@users.noreply.github.com>
2023-04-05 16:18:40 +0200

    New wrapper that works

--- main.py
+++ main.py
@@ -1,57 +1,72 @@
 import time
-from collections import defaultdict
-import logging
+from functools import wraps
 
 
-def time_it(make_report=False, group=None):
-    timing_data = defaultdict(list)
+class ExecutionTime:
+    results = {}
 
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            start_time = time.time()
+    def __init__(self, group_name):
+        self.group_name = group_name
+        self.func_name = None
+        self.start_time = None
+        self.end_time = None
+        self.execution_time = None
+
+    def __call__(self, func):
+        def wrapped_func(*args, **kwargs):
+            self.func_name = func.__name__
+            self.start_time = time.time()
             result = func(*args, **kwargs)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            timing_data[func.__name__].append(elapsed_time)
+            self.end_time = time.time()
+            self.execution_time = self.end_time - self.start_time
+            if self.group_name not in ExecutionTime.results:
+                ExecutionTime.results[self.group_name] = []
+            ExecutionTime.results[self.group_name].append((self.func_name, self.execution_time))
             return result
-        return wrapper
 
+        return wrapped_func
+
+    @staticmethod
     def report():
-        nonlocal make_report
-        make_report = True
-
-    def report_groups():
-        nonlocal make_report, group
-        make_report = True
-        group_data = defaultdict(list)
-        for name, times in timing_data.items():
-            group_data[name.split(group, 1)[0]].extend(times)
-        for name, times in group_data.items():
-            print(f"{name} group: Function {group}{name} took {min(times)} seconds to execute.")
-
-    if make_report:
-        if group is None:
-            return decorator
-        else:
-            return report_groups
-    else:
-        return report
+        for group_name, results in sorted(ExecutionTime.results.items()):
+            print(f"Group name {group_name}")
+            print("No.\tFunction Name\t\tExecution Time")
+            results = sorted(results, key=lambda x: x[1])
+            for i, (func_name, execution_time) in enumerate(results):
+                print(f"{i + 1}\t{func_name}\t\t{execution_time:.6f}")
+            print()
 
 
-def log_it(log_file):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+@ExecutionTime(group_name=1)
+def reverse_string_1(s):
+    return s[::-1]
 
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            logger.info(f"Function {func.__name__} was called with arguments {args}, {kwargs}, and returned {result}.")
-            return result
-        return wrapper
 
-    return decorator
+@ExecutionTime(group_name=1)
+def reverse_string_2(s):
+    return "".join(reversed(s))
+
+
+@ExecutionTime(group_name=1)
+def reverse_string_3(s):
+    return "".join(s[i] for i in range(len(s) - 1, -1, -1))
+
+
+@ExecutionTime(group_name=1)
+def reverse_string_4(s):
+    return "".join(s[i] for i in reversed(range(len(s))))
+
+
+@ExecutionTime(group_name=1)
+def reverse_string_5(s):
+    return "".join(s[i] for i in range(len(s) - 1, -1, -1))
+
+
+if __name__ == '__main__':
+    s = 'qwerty'
+    reverse_string_1(s)
+    reverse_string_2(s)
+    reverse_string_3(s)
+    reverse_string_4(s)
+    reverse_string_5(s)
+    ExecutionTime.report()
